3847d64b4ee80394554f9149ea98a12264dcf116
Naokichi55 <ibosanganba@gmail.com>
2025-08-09 17:05:50 +0900

    add:avataorカラムをstring型で追加

--- db/migrate/20250809075311_add_column_profiles.rb
+++ db/migrate/20250809075311_add_column_profiles.rb
@@ -1,4 +1,9 @@
 class AddColumnProfiles < ActiveRecord::Migration[7.2]
-  def change
+  def up
+    add_column :profiles, :avataor, :string
+  end
+
+  def down
+    remove_column :profiles, :avatar, :string
   end
 end
