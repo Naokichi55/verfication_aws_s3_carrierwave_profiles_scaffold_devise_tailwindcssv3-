aee5a12c91fde982acc6cc3e7c9a534b4f908b80
Naokichi55 <ibosanganba@gmail.com>
2025-08-10 13:08:42 +0900

    add:コメントを修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,12 +8,12 @@ CarrierWave.configure do |config|
  if Rails.env.production?
 	config.storage = :fog
 	config.fog.provider = 'fog/aws'
-	config.fog_directory = '<%= ENV['PROFILE_AWS_BUCKET'] %>'
+	config.fog_directory = '<% ENV['PROFILE_AWS_BUCKET'] %>'
 	config.fog.credentials{
-		provider: 'AWS'
-		aws_access_key_id: '<%= ENV['AWS_ACCESS_KEY_ID'] %>'
-		aws_secret_access_key: '<%= ENV['AWS_SECRET_ACCESS_KEY'] %>'
-		region: '<%= ENV['AWS_RESION'] %>'
+		provider: 'AWS',
+		aws_access_key_id: '<%= ENV['AWS_ACCESS_KEY_ID'] %>',
+		aws_secret_access_key: '<%= ENV['AWS_SECRET_ACCESS_KEY'] %>',
+		region: '<%= ENV['AWS_RESION'] %>',
 	}
 else
   config.storage :file #開発環境はpublic/uplodersに保存する
